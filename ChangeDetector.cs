bcb863bf993843de45e68f9e5047d2b9983a19d8
Elaviers <elaviers@gmail.com>
2020-11-25 15:04:56 -0800

    Initial Commit

---
+++ ChangeDetector.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace WebChangeDetect
+{
+    class ChangeDetector
+    {
+        private class Tracker
+        {
+            private class SubTracker
+            {
+                public int initialState;
+                private readonly string text;
+                
+                public SubTracker(string text)
+                {
+                    this.initialState = -1;
+                    this.text = text;
+                }
+
+                //Returns true if differs from initial state
+                public bool Check(string content)
+                {
+                    int state = content.IndexOf(text, StringComparison.OrdinalIgnoreCase) >= 0 ? 1 : 0;
+
+                    Console.ForegroundColor = state != 0 ? ConsoleColor.Green : ConsoleColor.Red;
+                    Console.Write('▓');
+
+                    if (initialState < 0) initialState = state;
+                    return initialState != state;
+                }
+            }
+
+            public string name;
+            public readonly string url;
+            private List<SubTracker> subtrackers;
+
+            public bool canNotify;
+
+            public Tracker(string url)
+            {
+                this.name = "";
+                this.url = url;
+                this.subtrackers = new List<SubTracker>();
+                this.canNotify = true;
+            }
+
+            public bool Check(HttpClient web)
+            {
+                HttpResponseMessage response = web.GetAsync(url).GetAwaiter().GetResult();
+                response.EnsureSuccessStatusCode();
+
+                string content = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                bool differsFromInitial = false;
+
+                foreach (var st in subtrackers)
+                    if (st.Check(content))
+                        differsFromInitial = true;
+
+                return differsFromInitial;
+            }
+
+            public void Add(string text)
+            {
+                subtrackers.Add(new SubTracker(text));
+            }
+
+            public void TrySetSTInitialState(bool initialState)
+            {
+                if (subtrackers.Count > 0)
+                    subtrackers[subtrackers.Count - 1].initialState = initialState ? 1 : 0;
+            }
+        }
+
+        private List<Tracker> trackers = new List<Tracker>();
+
+        public delegate void Notify(string name, string url);
+
+        //notify args are name and url
+        public void Check(HttpClient client, Notify notify)
+        {
+            foreach (Tracker e in trackers)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Out.Write($"{(e.name.Length > 0 ? e.name : e.url)} ");
+
+                bool differsFromInitial = e.Check(client);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Out.Write('|');
+
+                Console.ForegroundColor = differsFromInitial ? ConsoleColor.Green : ConsoleColor.Red;
+                Console.Out.Write("█ ");
+
+                if (differsFromInitial)
+                {
+                    if (e.canNotify)
+                    {
+                        e.canNotify = false;
+                        notify(e.name, e.url);
+                    }
+                }
+                else
+                {
+                    e.canNotify = true;
+                }
+            }
+        }
+
+        public void AddTracker(string url)
+        {
+            trackers.Add(new Tracker(url));
+        }
+
+        //Adds a check to the latest tracker
+        public void AddCheck(string check)
+        {
+            trackers[trackers.Count - 1].Add(check);
+        }
+
+        //Attempts to set name of latest tracker
+        public void TrySetTrackerName(string name)
+        {
+            if (trackers.Count > 0)
+                trackers[trackers.Count - 1].name = name;
+        }
+
+        //Attempts to set initial state of the latest tracker's latest subtracker
+        public void TrySetTrackerSTState(bool state)
+        {
+            if (trackers.Count > 0)
+                trackers[trackers.Count - 1].TrySetSTInitialState(state);
+        }
+
+        public bool IsEmpty { get { return trackers.Count == 0; } }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Media;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace WebChangeDetect
+{
+    class Program
+    {
+        class Entry
+        {
+            public string url;
+            public List<string> changestrings;
+
+            public Entry(string url)
+            {
+                this.url = url;
+                this.changestrings = new List<string>();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+            MatchCollection m = Regex.Matches(argstring, @"""((?:\\.|[^""])*)""|\S+");
+            
+            for (int i = 0; i < m.Count; ++i)
+            {
+                string arg = m[i].Groups[1].Length > 0 ? m[i].Groups[1].Value : m[i].Value;
+            */
+
+            float interval = 60f;
+            ChangeDetector cd = new ChangeDetector();
+            bool newurl = true;
+            string flag = null;
+            for (int i = 0; i < args.Length; ++i)
+            {
+                string arg = args[i];
+
+                if (arg[0] == '/')
+                {
+                    if (arg.Length == 1)
+                    {
+                        newurl = true;
+                        continue;
+                    }
+
+                    flag = arg.Substring(1);
+
+                    if (flag.Equals("?"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("WinWebDetect");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("Now 100% legal-ish");
+
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine(
+@"
+Usage:
+    WinWebDetect [/i INTERVAL] URL [TRACKERS...] [/ URL [TRACKERS...] [/ URL [TRACKERS...] - ...]]
+    
+    Where URL is a URL to request and TRACKERS are strings to look for in the request
+    
+
+Flags:
+    /i, /interval       Amount of time between each check
+    /s                  Marks the desired state of the current tracker as FALSE
+    /S                  Marks the desired state of the current tracker as TRUE
+    /n, /name           Name of the current url");
+
+
+                        return;
+                    }
+                    if (flag.Equals("s"))
+                    {
+                        cd.TrySetTrackerSTState(true);
+                        flag = null;
+                    }
+                    else if (flag.Equals("S"))
+                    {
+                        cd.TrySetTrackerSTState(false);
+                        flag = null;
+                    }
+                }
+                else
+                {
+                    if (flag != null)
+                    {
+                        if (flag.Equals("interval", StringComparison.OrdinalIgnoreCase) || flag.Equals("i", StringComparison.OrdinalIgnoreCase))
+                        {
+                            float.TryParse(arg, out interval);
+                            flag = null;
+                        }
+                        else if (flag.Equals("name", StringComparison.OrdinalIgnoreCase) || flag.Equals("n", StringComparison.OrdinalIgnoreCase))
+                        {
+                            cd.TrySetTrackerName(arg);
+                            flag = null;
+                        }
+                        else
+                            flag = null;
+                    }
+                    else
+                    {
+                        if (newurl)
+                        {
+                            if (!arg.Substring(0, 4).Equals("http", StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.Write($"\"{arg}\"->");
+                                arg = "https://" + arg;
+                                Console.WriteLine($"\"{arg}\"");
+                            }
+
+                            cd.AddTracker(arg);
+                            newurl = false;
+                        }
+                        else
+                            cd.AddCheck(arg);
+                    }
+                }
+            }
+
+            if (cd.IsEmpty)
+            {
+                Console.WriteLine("No URLS provided, exiting..");
+                return;
+            }
+
+            SoundPlayer player = new SoundPlayer("./alert.wav");
+
+            int msInterval = (int)(interval * 1000);
+            HttpClient client = new HttpClient();
+            while (true)
+            {
+                var date = DateTime.Now;
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Out.Write($"{date.Month:d02}/{date.Day:d02} {date.Hour:d02}:{date.Minute:d02}:{date.Second:d02} ");
+
+                cd.Check(client, 
+                    (string name, string url) => {
+                        player.PlayLooping();
+
+                        string fname = name.Length > 0 ? $"\"{name} ({url})\"" : url;
+
+                        var result = MessageBox.Show(
+                            $"{fname} has been updated.\nGo there now?",
+                            $"Change detected for {fname}",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+
+                        player.Stop();
+
+                        if (result == DialogResult.Yes)
+                        {
+                            System.Diagnostics.Process.Start(url);
+                        }
+                    });
+
+                Console.Out.WriteLine();
+                
+                System.Threading.Thread.Sleep(msInterval);
+            }
+        }
+    }
+}
